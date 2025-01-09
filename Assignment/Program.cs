13dbd5ce50fe04b44e22dd590bbd3cc16098d6b0
mahbahig <mahbahig2@gmail.com>
2025-01-09 17:59:44 +0200

    Q3

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -1,61 +1,80 @@
 ﻿namespace Assignment
 {
-    class MyClass
-    {
-        public int Value { get; set; }
-    }
+    //class MyClass
+    //{
+    //    public int Value { get; set; }
+    //}
     internal class Program
     {
         static void Main(string[] args)
         {
             #region Q1
-            int a = 5;
-            Console.WriteLine($"Before passing by value: {a}");
-            PassByValue(a);
-            Console.WriteLine($"After passing by value: {a}");
+            //int a = 5;
+            //Console.WriteLine($"Before passing by value: {a}");
+            //PassByValue(a);
+            //Console.WriteLine($"After passing by value: {a}");
 
-            int b = 5;
-            Console.WriteLine($"Before passing by reference: {b}");
-            PassByReference(ref b);
-            Console.WriteLine($"After passing by reference: {b}");
+            //int b = 5;
+            //Console.WriteLine($"Before passing by reference: {b}");
+            //PassByReference(ref b);
+            //Console.WriteLine($"After passing by reference: {b}");
             #endregion
 
             #region Q2
-            MyClass obj1 = new MyClass { Value = 5 };
-            Console.WriteLine($"Before passing by value: {obj1.Value}");
-            PassByValue(obj1);
-            Console.WriteLine($"After passing by value: {obj1.Value}");
+            //MyClass obj1 = new MyClass { Value = 5 };
+            //Console.WriteLine($"Before passing by value: {obj1.Value}");
+            //PassByValue(obj1);
+            //Console.WriteLine($"After passing by value: {obj1.Value}");
 
-            MyClass obj2 = new MyClass { Value = 5 };
-            Console.WriteLine($"Before passing by reference: {obj2.Value}");
-            PassByReference(ref obj2);
-            Console.WriteLine($"After passing by reference: {obj2.Value}");
+            //MyClass obj2 = new MyClass { Value = 5 };
+            //Console.WriteLine($"Before passing by reference: {obj2.Value}");
+            //PassByReference(ref obj2);
+            //Console.WriteLine($"After passing by reference: {obj2.Value}");
+            #endregion
+
+            #region Q3
+            Console.WriteLine("Enter four numbers:");
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int c = int.Parse(Console.ReadLine());
+            int d = int.Parse(Console.ReadLine());
+            var result = addTwoSubstractTwo(a, b, c, d);
+            Console.WriteLine($"Sum: {result.sum} And Difference: {result.diff}");
             #endregion
         }
 
         #region Value Type Methods
-        static void PassByValue(int x)
-        {
-            x = 10;
-        }
+        //static void PassByValue(int x)
+        //{
+        //    x = 10;
+        //}
 
-        static void PassByReference(ref int x)
-        {
-            x = 10;
-        }
+        //static void PassByReference(ref int x)
+        //{
+        //    x = 10;
+        //}
         #endregion
 
         #region Reference Type Methods
-        static void PassByValue(MyClass obj)
-        {
-            obj.Value = 10;
-            obj = new MyClass { Value = 20 };
-        }
+        //static void PassByValue(MyClass obj)
+        //{
+        //    obj.Value = 10;
+        //    obj = new MyClass { Value = 20 };
+        //}
 
-        static void PassByReference(ref MyClass obj)
+        //static void PassByReference(ref MyClass obj)
+        //{
+        //    obj.Value = 10;
+        //    obj = new MyClass { Value = 20 };
+        //}
+        #endregion
+
+        #region Q3
+        static (int sum, int diff) addTwoSubstractTwo(int a, int b, int c, int d)
         {
-            obj.Value = 10;
-            obj = new MyClass { Value = 20 };
+            int sum = a + b;
+            int diff = c - d;
+            return (sum, diff);
         }
         #endregion
     }
